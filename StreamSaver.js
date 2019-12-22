f036a91045cc356ef02b8773062ddad8f413c17d
kevin odwyer <kevodwyer@gmail.com>
2019-12-22 17:33:09 +0000

    initial

---
+++ StreamSaver.js
@@ -0,0 +1,166 @@
+/* global location WritableStream ReadableStream define MouseEvent MessageChannel TransformStream */
+;((name, definition) => {
+  typeof module !== 'undefined'
+    ? module.exports = definition()
+    : typeof define === 'function' && typeof define.amd === 'object'
+      ? define(definition)
+      : this[name] = definition()
+})('streamSaver', () => {
+  'use strict'
+
+  const secure = location.protocol === 'https:' || location.hostname === 'localhost'
+  let iframe
+  let loaded
+  let transfarableSupport = false
+  let streamSaver = {
+    createWriteStream,
+    supported: false,
+    version: {
+      full: '1.1.0',
+      major: 1,
+      minor: 1,
+      dot: 0
+    }
+  }
+
+  streamSaver.worker = 'worker.html?version=' +
+    streamSaver.version.full
+
+  try {
+    // Some browser has it but ain't allowed to construct a stream yet
+    streamSaver.supported = 'serviceWorker' in navigator && !!new ReadableStream() && !!new WritableStream()
+  } catch (err) {}
+
+  try {
+    const { readable } = new TransformStream()
+    const mc = new MessageChannel()
+    mc.port1.postMessage(readable, [readable])
+    mc.port1.close()
+    mc.port2.close()
+    transfarableSupport = readable.locked === true
+  } catch (err) {
+    // Was first enabled in chrome v73
+  }
+
+  function createWriteStream (filename, mimeType, urlCallback, seekCallback, queuingStrategy, size, fileDataCallback) {
+    // normalize arguments
+    if (Number.isFinite(queuingStrategy)) {
+      [size, queuingStrategy] = [queuingStrategy, size]
+    }
+
+    let channel = new MessageChannel()
+    let popup
+    let setupChannel = readableStream => new Promise(resolve => {
+	const args = [ { filename, size, mimeType }, '*', [ channel.port2 ] ]
+
+      // Pass along transfarable stream
+      if (readableStream) {
+        args[0].readableStream = readableStream
+        args[2].push(readableStream)
+      }
+
+      channel.port1.onmessage = evt => {
+        // Service worker sent us a link from where
+        // we recive the readable link (stream)
+        if (evt.data.download) {
+          resolve() // Signal that the writestream are ready to recive data
+          if (!secure) popup.close() // don't need the popup any longer
+            urlCallback(evt.data.download)
+
+            /*
+          // Cleanup
+          if (readableStream) {
+            // We don't need postMessages now when stream are transferable
+            channel.port1.close()
+            channel.port2.close()
+          }
+
+          channel.port1.onmessage = null
+          */
+        } else if (evt.data.filePath) {
+            fileDataCallback(evt.data.filePath)
+        } else if (evt.data.seekLength) {
+            seekCallback(evt.data.seekHi, evt.data.seekLo, evt.data.seekLength)
+        }
+      }
+
+      if (secure && !iframe) {
+        iframe = document.createElement('iframe')
+        iframe.src = streamSaver.worker
+        iframe.hidden = true
+        document.body.appendChild(iframe)
+      }
+
+      if (secure && !loaded) {
+        let fn
+        iframe.addEventListener('load', fn = () => {
+          loaded = true
+          iframe.removeEventListener('load', fn)
+          iframe.contentWindow.postMessage(...args)
+        })
+      }
+
+      if (secure && loaded) {
+        iframe.contentWindow.postMessage(...args)
+      }
+
+      if (!secure) {
+        popup = window.open(streamSaver.worker, Math.random())
+        let onready = evt => {
+          if (evt.source === popup) {
+            popup.postMessage(...args)
+            window.removeEventListener('message', onready)
+          }
+        }
+
+        // Another problem that cross origin don't allow is scripting
+        // so popup.onload() don't work but postMessage still dose
+        // work cross origin
+        window.addEventListener('message', onready)
+      }
+    })
+
+    if (transfarableSupport) {
+      const ts = new TransformStream({
+        start () {
+          return new Promise(resolve =>
+            setTimeout(() => setupChannel(ts.readable).then(resolve))
+          )
+        }
+      }, queuingStrategy)
+
+      return ts.writable
+    }
+
+    return new WritableStream({
+      start () {
+        // is called immediately, and should perform any actions
+        // necessary to acquire access to the underlying sink.
+        // If this process is asynchronous, it can return a promise
+        // to signal success or failure.
+        return setupChannel()
+      },
+      write (chunk) {
+        // is called when a new chunk of data is ready to be written
+        // to the underlying sink. It can return a promise to signal
+        // success or failure of the write operation. The stream
+        // implementation guarantees that this method will be called
+        // only after previous writes have succeeded, and never after
+        // close or abort is called.
+
+        // TODO: Kind of important that service worker respond back when
+        // it has been written. Otherwise we can't handle backpressure
+        // EDIT: Transfarable streams solvs this...
+        channel.port1.postMessage(chunk)
+      },
+      close () {
+        channel.port1.postMessage('end')
+      },
+      abort () {
+        channel.port1.postMessage('abort')
+      }
+    }, queuingStrategy)
+  }
+
+  return streamSaver
+})
---
+++ sw.js
@@ -0,0 +1,124 @@
+/* global self ReadableStream Response */
+
+const downloadMap = new Map()
+var fileDataTuple
+var webViewerPrefix = "/sandbox/";
+// This should be called once per download
+// Each event has a dataChannel that the data will be piped through
+self.onmessage = event => {
+  // We send a heartbeat every x secound to keep the
+  // service worker alive
+  if (event.data === 'ping') {
+    return
+  }
+
+  // Create a uniq link for the download
+  const uniqLink = self.registration.scope + 'intercept-me-nr' + Math.random()
+  const port = event.ports[0]
+
+  var filename = event.data.filename
+  var headers
+  if (filename.startsWith("web-viewer")) {
+        let fileData = new FileData();
+        setupStreamingFileData(port, fileData)
+        fileDataTuple = [fileData, port]
+  } 
+  port.postMessage({ download: uniqLink, ping: self.registration.scope + 'ping' })
+
+  // Mistage adding this and have streamsaver.js rely on it
+  // depricated as from 0.2.1
+  port.postMessage({ debug: 'Mocking a download request' })
+}
+
+function FileData() {
+    //TODO leaks memory
+     this.fileMap = new Map()
+
+    this.isComplete = function() {
+        return this.complete
+    }
+    this.setComplete = function() {
+        this.complete = true
+    }
+    this.getFile = function(fullPath) {
+        return this.fileMap.get(fullPath)
+    }
+    //currently assume each resource fits in 1 chunk
+    //header consists of  filePathSize (1 byte), filePath
+    this.enqueue = function(moreData) {
+        let filePathSize = moreData[0];
+        let filePathBytes = moreData.subarray(1, filePathSize + 1);
+        let filePath = new TextDecoder().decode(filePathBytes);
+
+        var file = this.fileMap.get(filePath)
+        if(file == null) {
+            file = new Uint8Array(0);
+        }
+        const combinedSize = file.byteLength + moreData.byteLength - filePathSize;
+        var newFile = new Uint8Array(combinedSize);
+        newFile.set(file);
+        newFile.set(moreData.subarray(filePathSize + 1), file.byteLength);
+        this.fileMap.set(filePath, newFile)
+    }
+}
+function setupStreamingFileData(port, fileData) {
+    port.onmessage = ({ data }) => {
+        if (data != 'end' && data != 'abort') {
+            if (data.byteLength == 0) {
+                fileData.setComplete()
+            } else {
+                fileData.enqueue(data)
+            }
+        }
+    }
+}
+
+self.addEventListener('install', event =>  {
+    self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+    clients.claim();
+});
+
+const maxBlockSize = 1024 * 1024 * 5;
+
+self.onfetch = event => {
+    const url = event.request.url
+    console.log("url=" + url);
+    if (url.endsWith('/ping')) {
+      return event.respondWith(new Response('pong', {
+        headers: { 'Access-Control-Allow-Origin': '*' }
+      }))
+    }
+    if (url.includes(webViewerPrefix)) {
+            var filePath = url.substring(url.indexOf(webViewerPrefix) + webViewerPrefix.length);
+            console.log("sw filePath=" + filePath);
+            const [fileData, port] = fileDataTuple
+            port.postMessage({ filePath: filePath })
+            return event.respondWith(returnFileData(fileData, filePath))
+    } else {
+		return;
+    }
+}
+
+function returnFileData(fileData, filePath) {
+    return new Promise(function(resolve, reject) {
+        let pump = () => {
+            let file = fileData.getFile(filePath)
+            if (file == null || file.byteLength == 0) {
+                setTimeout(pump, 500)
+            } else {
+                resolve(file);
+            }
+        }
+        pump()
+    }).then(function(arrayBuffer, err) {
+            return new Response(arrayBuffer, {
+              status: 200,
+              headers: [
+                ['content-length', arrayBuffer.byteLength]
+              ]
+            });
+    });
+}
+
